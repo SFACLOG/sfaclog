d3e8991cf527c713b6a2c6b14d1f99c32050d142
thwlckd <thwlckd@naver.com>
2024-01-24 17:11:00 +0900

    Feat: pocketbase user api 작업 - user 관련 메소드 작업 - 폴더 구조 수정

--- apps/web/src/api/user.ts
+++ apps/web/src/api/user.ts
@@ -4,10 +4,10 @@ const pb = new PocketBase('http://3.38.183.51:8090');
 
 export const login = async (id: string, password: string) => {
   await pb.collection('user').authWithPassword(id, password);
+};
 
-  console.log(pb.authStore.isValid);
-  console.log(pb.authStore.token);
-  console.log(pb.authStore.model?.id);
+export const logout = () => {
+  pb.authStore.clear();
 };
 
 export const signup = async (data: {
@@ -25,6 +25,19 @@ export const signup = async (data: {
   //   await pb.collection('user').requestVerification(data.email);
 };
 
-export const signout = async (id: string) => {
+// 회원탈퇴 기능 수정 필요
+export const withdrawal = async (id: string) => {
   await pb.collection('user').delete(id);
+};
+
+export const getUser = () => {
+  return pb.authStore.model;
+};
+
+export const isValidUser = () => {
+  return pb.authStore.isValid;
+};
+
+export const getToken = () => {
+  return pb.authStore.token;
 };

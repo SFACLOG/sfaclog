5f5915455415ac48b236ff664b5b7b729bf13388
thwlckd <thwlckd@naver.com>
2024-01-29 23:04:18 +0900

    Feat: Input value 재검증 스토리 작성

--- packages/sfac-design-kit/src/components/Form/Input.stories.tsx
+++ packages/sfac-design-kit/src/components/Form/Input.stories.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import { Input } from './Input';
 
 export default {
@@ -50,3 +51,51 @@ export const All = () => (
     />
   </div>
 );
+
+export const Validation = () => {
+  const passwordRef1 = useRef<HTMLInputElement>(null);
+  const passwordValidationRef = useRef<HTMLInputElement>(null);
+  const [inputStatus, setInputStatus] = useState<
+    'normal' | 'success' | 'error'
+  >('normal');
+  const [inputDescription, setInputDescription] = useState<string>();
+
+  const handleOnChange = () => {
+    if (passwordRef1.current && passwordValidationRef.current) {
+      setInputStatus(() => {
+        if (
+          passwordRef1.current?.value === passwordValidationRef.current?.value
+        ) {
+          setInputDescription(undefined);
+          return 'success';
+        } else {
+          setInputDescription('비밀번호가 일치하지 않습니다.');
+          return 'error';
+        }
+      });
+    }
+  };
+
+  return (
+    <div className='flex flex-col gap-4 w-96'>
+      <Input
+        type='password'
+        minLength={8}
+        label='비밀번호'
+        required
+        placeholder='(필수) 비밀번호 입력'
+        ref={passwordRef1}
+      />
+      <Input
+        type='password'
+        label='비밀번호 확인'
+        required
+        placeholder='(필수) 비밀번호 입력'
+        description={inputDescription || undefined}
+        status={inputStatus}
+        onChange={handleOnChange}
+        ref={passwordValidationRef}
+      />
+    </div>
+  );
+};

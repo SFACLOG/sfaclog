325c7de12fd9ad0e0ac9d3d1899cd82d1f4b74f3
thwlckd <thwlckd@naver.com>
2024-01-31 15:37:00 +0900

    Feat: LogLargeCard 컴포넌트 및 스토리 작성

---
+++ packages/sfac-design-kit/src/components/Card/LogLargeCard/index.tsx
@@ -0,0 +1,52 @@
+import { Chip } from '@/components/Chip';
+import { cn } from '@/utils';
+import { Children } from 'react';
+
+export interface LogLargeCardProps {
+  width?: string;
+  thumbnail: string;
+  tags?: string[];
+  title: string;
+  summary: string;
+  likes: number;
+  comments: number;
+}
+
+export const LogLargeCard = ({
+  width,
+  thumbnail,
+  tags,
+  title,
+  summary,
+  likes,
+  comments,
+}: LogLargeCardProps) => {
+  return (
+    <div className={cn('flex flex-col gap-5 w-full max-h-[225px]', width)}>
+      <div className='flex gap-5 max-h-[180px]'>
+        <img
+          className='w-[265px] object-cover rounded-[10px]'
+          src={thumbnail}
+          alt='thumbnail'
+        />
+        <div className='flex flex-col gap-[25px] w-full h-full whitespace-pre-wrap'>
+          <p className='text-h2 line-clamp-2'>{title}</p>
+          <p className='text-body2 text-neutral-80 line-clamp-5'>{summary}</p>
+        </div>
+      </div>
+      <div className='flex items-center justify-between'>
+        {tags && (
+          <div className='flex gap-[5px]'>
+            {Children.toArray(tags.map(tag => <Chip># {tag}</Chip>))}
+          </div>
+        )}
+        <div className='flex items-center text-caption2'>
+          <img src='/images/dark_heart.svg' />
+          <span className='ml-[7px]'>{likes}</span>
+          <img className='ml-6' src='/images/chat.svg' />
+          <span className='ml-[7px]'>{comments}</span>
+        </div>
+      </div>
+    </div>
+  );
+};

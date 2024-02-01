65ee649eb280883b1f267c39fffbad8a3f43c5a9
seonnn <ssni0105@gmail.com>
2024-02-01 23:52:20 +0900

    Feat: ImageWrapper 컴포넌트 추가

---
+++ packages/sfac-design-kit/src/components/common/ImageWrapper.tsx
@@ -0,0 +1,11 @@
+import React, { ImgHTMLAttributes } from 'react';
+
+interface ImageWrapperProps extends ImgHTMLAttributes<HTMLImageElement> {
+  path: string | { src: string };
+}
+
+const ImageWrapper = ({ path, ...props }: ImageWrapperProps) => {
+  return <img src={typeof path === 'string' ? path : path.src} {...props} />;
+};
+
+export default ImageWrapper;

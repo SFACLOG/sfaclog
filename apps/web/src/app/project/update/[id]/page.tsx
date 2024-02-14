05dac82b186de9a460363d0069a8f887e9b5f1c7
TaeYoungPar <tpark@oswego.edu>
2024-02-15 05:00:33 +0900

    Feat: 수정하기 이미지 갯수 제한

--- apps/web/src/app/project/update/[id]/page.tsx
+++ apps/web/src/app/project/update/[id]/page.tsx
@@ -9,7 +9,7 @@ import {
 } from 'sfac-design-kit';
 import GoBack from '../../(components)/GoBack';
 import Calendar from '../../(components)/Calendar';
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import { getUser, getUserId } from '@/api/user';
 import { updateProjectId } from '@/api/project';
@@ -40,6 +40,8 @@ import { useGetSkillData } from '@/hooks/useSkillData';
 import { useGetPositionData } from '@/hooks/usePositionData';
 import { useGetMeetingData } from '@/hooks/useMeetingData';
 import { imagechipoptions, process, position } from '../../(contants)';
+
+const formData = new FormData();
 
 const page = () => {
   const id = usePathname();
@@ -79,6 +81,10 @@ const page = () => {
   );
 
   const textRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    setPreviews(projectImage || []);
+  }, [projectImage]);
 
   const handleProcessChange = (selectedOption: SelectBoxOption) => {
     setSelectedProcess(selectedOption.value);
@@ -138,7 +144,7 @@ const page = () => {
     for (let i = 0; i < e.target.files!.length; i++) {
       const file: File = e.target.files![i];
       // 이미지 파일 3개로 제한
-      if (newImages.length < 3) {
+      if (newPreviews.length < 3) {
         // 이벤트객체의 파일을 newImages에 담기
         newImages.push(file);
         // 파일리더 객체 생성
@@ -326,7 +332,6 @@ const page = () => {
     { label: '9명', value: '9명' },
     { label: '10명 이상', value: '10명 이상' },
   ];
-
   return (
     <div className='mt-[50px] max-w-[780px] mx-auto'>
       <GoBack />
@@ -446,7 +451,7 @@ const page = () => {
           />
 
           <div>
-            {previews?.map((preview, index) => (
+            {previews.map((preview, index) => (
               <div key={index} className='mb-5 rounded-[20px] relative'>
                 <Image
                   src={preview}
